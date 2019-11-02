6dd5c2c1c7a5206113dccac8ca643e8bb3184cf1
Pablo Cabezas <pabcabsal@gmail.com>
2019-11-02 11:35:43 +0100

    Refactor

--- configclasses/helpers.py
+++ configclasses/helpers.py
@@ -32,14 +32,7 @@ def fill_init_dict(
 ):
     init_dict = {}
     for class_field_name, class_field_type, class_field_default in class_fields:
-        if not prefix and not parent_field_name:
-            origin_field_name = class_field_name
-        elif parent_field_name:
-            origin_field_name = f"{parent_field_name}_{class_field_name}"
-        elif prefix:
-            origin_field_name = f"{prefix}_{class_field_name}"
-        else:
-            origin_field_name = f"{prefix}_{parent_field_name}_{class_field_name}"
+        origin_field_name = get_origin_field_name(class_field_name, parent_field_name, prefix)
 
         if is_dataclass(class_field_type):
             init_dict[class_field_name] = class_field_type.from_environ(
@@ -48,18 +41,34 @@ def fill_init_dict(
         elif field_value := get_field_value_from_environ(
             origin_field_name
         ) or get_default_value(origin_field_name, defaults):
-            if class_field_type in converter_types:
-                init_dict[class_field_name] = class_field_type(field_value)
-            elif class_field_type == bool:
-                init_dict[class_field_name] = (
-                    field_value == "True" or field_value == "true"
-                )
-            else:
-                init_dict[class_field_name] = field_value
+            fill_with_environ_or_provided_defaults(class_field_name, class_field_type, field_value, init_dict)
         else:
             init_dict[class_field_name] = class_field_default
 
     return init_dict
+
+
+def fill_with_environ_or_provided_defaults(class_field_name, class_field_type, field_value, init_dict):
+    if class_field_type in converter_types:
+        init_dict[class_field_name] = class_field_type(field_value)
+    elif class_field_type == bool:
+        init_dict[class_field_name] = (
+                field_value == "True" or field_value == "true"
+        )
+    else:
+        init_dict[class_field_name] = field_value
+
+
+def get_origin_field_name(class_field_name, parent_field_name, prefix):
+    if not prefix and not parent_field_name:
+        origin_field_name = class_field_name
+    elif parent_field_name:
+        origin_field_name = f"{parent_field_name}_{class_field_name}"
+    elif prefix:
+        origin_field_name = f"{prefix}_{class_field_name}"
+    else:
+        origin_field_name = f"{prefix}_{parent_field_name}_{class_field_name}"
+    return origin_field_name
 
 
 def path_to_env(path: Path):
